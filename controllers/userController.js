8ce12c94917da80796d6a450ce5415c26126b124
Sheryl L <s_lawrence0@yahoo.ca>
2024-05-06 14:58:02 -0400

    Creating a new user code added to userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -29,5 +29,15 @@ module.exports = {
             res.status(500).json(err);
         }
     },
+
+    // Create a new user
+    async createUser(req, res) {
+        try {
+            const user = await User.create(req.body);
+            res.json(user);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
     
     }
