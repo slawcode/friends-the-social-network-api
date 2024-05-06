6eea19005452ccc271a615a7aaf03ffd01ac81a8
Sheryl L <s_lawrence0@yahoo.ca>
2024-05-06 17:02:34 -0400

    Code finished for user schema in user.js

--- models/User.js
+++ models/User.js
@@ -9,12 +9,36 @@ const user Schema = new Schema(
             required: true,
             uniqure: true,
             trim: true,
-        }
+        },
         email: {
             type: String,
             required: true,
             unique: true,
-            match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please provide a valid email address'],
-        }
+            match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, "Please provide a valid email address."], // Email validation 
+        },
+        thoughts: [ 
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought',
+            }
+        ],
+
+        friends: [
+            {
+                type: Schema. Types.objectId,
+                ref: 'User',
+            }
+        ],
     }
-)
+);
+
+// Virtual 'friendCount' to retrieve the length of the user's friends / Number of friends
+userSchema.virtual('friendCount').get(function(){
+    return this.friends.length;
+});
+
+// Create User model using the userSchema
+const User = model('User', userSchema)
+
+// Export the User model
+module.exports = User 
