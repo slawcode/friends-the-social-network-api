d7f62c357659ccba6f2d1f60516e95d76a875ef8
Sheryl L <s_lawrence0@yahoo.ca>
2024-05-06 14:25:18 -0400

    Get all users and get one user code started in userController.js

---
+++ controllers/userController.js
@@ -0,0 +1,33 @@
+const { User, Thought } = require("../models");
+
+module.exports = {
+
+// Get all users    
+    async getAllUsers(req, res) {
+        try {
+            const user = await User.find();
+            res.json(user);
+        }   catch (err) {
+            res.status(500).json(err);
+        }
+    },
+
+// Get one user by ID 
+    async getSingleUser(req, res) {
+        try {
+            const user = await User.findOne({ _id: req.params.userId })
+            .select('-__v')
+            .populate("thoughts")
+            .populate("friends")
+
+            if (!user) {
+                return res.status(404).json({ message: "No user with that ID!" });
+            }
+
+            res.json(user);
+        }   catch (err) {
+            res.status(500).json(err);
+        }
+    },
+    
+    }
