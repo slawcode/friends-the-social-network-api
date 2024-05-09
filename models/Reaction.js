e7b905307ea49170edd25d1defceff0369d5997f
Sheryl L <s_lawrence0@yahoo.ca>
2024-05-08 22:00:46 -0400

    Schema to create Reaction model completed in Reaction.js

--- models/Reaction.js
+++ models/Reaction.js
@@ -1,5 +1,6 @@
 const { Schema, Types} = require('mongoose');
 
+// Schema to create Reaction model
 const reactionSchema = new Schema (
     {
         reactionId: {
@@ -10,6 +11,26 @@ const reactionSchema = new Schema (
             type: Schema.Types.ObjectId,
             default: () => new Types.ObjectId(),
         },
-        reactionBody: 
+        reactionBody: {
+            type: String,
+            required: true,
+            maxLength: 280,
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+        },
+    },
+    {
+        toJSON: {
+            getters: true,
+        },
+        id: false,
     }
-)
+);
+
+module.exports = reactionSchema;
